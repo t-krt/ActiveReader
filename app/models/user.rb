5b48725d48ae19618d111f8eceed65e20837bf20
t-krt <cest1la6vieg@gmail.com>
2019-06-24 00:51:02 +0900

    Modify: ニックネームに一意性制約の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :avatar, AvatarUploader
 
-  validates :nickname, presence: true
+  validates :nickname, presence: true, uniqueness: true
 end
