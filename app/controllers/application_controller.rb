9c61a2b393c074f96acefe3dd53aa355404140e6
t-krt <cest1la6vieg@gmail.com>
2019-06-23 23:41:07 +0900

    Add: プロフィール画像の登録機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :avatar])
+    end
+
+  private
+    def after_sign_out_path_for(resource)
+      new_user_session_path
+    end
 end
