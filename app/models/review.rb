1b4ee9912311be857ebd7b329593ee5fefca80f3
t-krt <cest1la6vieg@gmail.com>
2019-06-26 21:53:35 +0900

    Add: レビュー投稿機能
    レビュー投稿機能の作成

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review < ApplicationRecord
+  has_many :tasks
+  accepts_nested_attributes_for :tasks
+
   belongs_to :user
-  begonds_to :book
+  belongs_to :book
+
+  validates :purpose, presence: true
+  validates :review_status, presence: true
+  validates :user_id, presence: true
+  validates :book_id, presence: true
+
+  enum review_status: [:reading, :read, :stock]
 end
