2a7d08ae69c14ef369bd729d1dcdcaf3bba19b2a
t-krt <cest1la6vieg@gmail.com>
2019-06-30 16:31:31 +0900

    Add: User#showのビューを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
+    @reading_book = Review.includes(:book, :user).find_by(user_id: current_user.id, review_status: "reading")
+    # @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
   
 end
