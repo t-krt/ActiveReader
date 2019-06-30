53c9e1cf0c433680b40522708ffae50cd57d32d8
t-krt <cest1la6vieg@gmail.com>
2019-06-30 19:16:26 +0900

    Fix: webpackerの設定ファイル修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,5 +5,3 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
-
-environment.loaders.get('vue').use[0].options.extractCSS = false
