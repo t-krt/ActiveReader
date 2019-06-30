d97341fe751ea5be228ace066ec2d714311fb9e2
t-krt <cest1la6vieg@gmail.com>
2019-06-30 19:15:20 +0900

    Add: 登録済みタスクを表示するビュー作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,12 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    has_task_reviews = Review.where(id: Task.select(:review_id).group(:review_id).having("count(review_id)>=?", 1))
+    @reviews = has_task_reviews.where(user_id: current_user.id, ).page(params[:page]).per(5).order("created_at DESC").includes(:book, :tasks)
   end
   
   def new
   end
+
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :review
+
+  validates :task_content, presence: true
 end
