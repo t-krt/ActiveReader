0d0ecb30acfc4bbb18c49b11ceda5162ec389429
t-krt <cest1la6vieg@gmail.com>
2019-07-02 21:56:13 +0900

    Add: 各読書状況のビューを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_in_path_for(resource)
-      user_path(current_user)
+      reading_path(current_user)
     end
 
 end
