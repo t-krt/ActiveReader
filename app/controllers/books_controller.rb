d46a961f8dbb487546c4eb2ddc849fcc1a45d31d
t-krt <cest1la6vieg@gmail.com>
2019-07-05 22:49:38 +0900

    Add: 書籍検索機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,35 @@
 class BooksController < ApplicationController
-  
   def search
+    @books = []
+
+    @title = params[:title]
+    if @title.present?
+      results = RakutenWebService::Books::Book.search({
+        title: @title,
+        hits: 20
+      })
+
+      results.each do |result|
+        book = Book.new(read(result))
+        @books << book
+      end
+    end
   end
 
+  private
+  def read(result)
+    title = result['title']
+    author = result['author']
+    url = result['itemUrl']
+    isbn = result['isbn']
+    image_url = result['mediumImageUrl']
+
+    {
+      title: title,
+      author: author,
+      url: url,
+      isbn: isbn,
+      remote_image_url: image_url,
+    }
+  end
 end
