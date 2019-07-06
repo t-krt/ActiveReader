921193232b441b446cb955dd717d474824cc5a4f
t-krt <cest1la6vieg@gmail.com>
2019-07-07 00:30:59 +0900

    Add: 投稿一覧の作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ApplicationController
   before_action :set_review_with_book, only: [:edit, :update, :show, :destroy]
 
   def index
+    @reviews = Review.includes(:book, :user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
