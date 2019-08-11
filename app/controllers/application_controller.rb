dcda5d686ed606a3ce1ec7da7ab44ec517c6b716
t-krt <cest1la6vieg@gmail.com>
2019-08-11 11:13:18 +0900

    引数の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,11 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for(_resource)
     new_user_session_path
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     reading_path(current_user)
   end
 end
