a72c2b9053b33bbfd1f20c1a5a21f619aa67fd8b
t-krt <cest1la6vieg@gmail.com>
2019-08-11 10:46:38 +0900

    不要なインデントの削除（application.controller.rb）
    'Layout/IndentationConsistency: Inconsistent indentation detected.'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,18 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :avatar])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :avatar])
+  end
 
   private
 
-    def after_sign_out_path_for(resource)
-      new_user_session_path
-    end
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 
-    def after_sign_in_path_for(resource)
-      reading_path(current_user)
-    end
+  def after_sign_in_path_for(resource)
+    reading_path(current_user)
+  end
 end
