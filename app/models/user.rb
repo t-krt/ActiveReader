4153d79914f4cb2478ad95e91f8bd1483356552c
t-krt <cest1la6vieg@gmail.com>
2019-08-11 11:25:13 +0900

    userモデルのdependentオプションの追加
    ユーザーが削除されたときに、子要素であるレビューもすべて削除されるようにしました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :avatar, AvatarUploader
 
-  has_many :reviews
+  has_many :reviews, dependent: :delete_all
   validates :nickname, presence: true, uniqueness: true
 end
