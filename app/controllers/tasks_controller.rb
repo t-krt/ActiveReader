40cfaeb4ee78519c75773d986fc8d3c99bbd8b41
t-krt <cest1la6vieg@gmail.com>
2019-08-11 11:56:03 +0900

    引数内の不要なコンマの削除
    'Style/TrailingCommaInArguments: Avoid comma after the last parameter of a method call.'

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
 
   def index
     has_task_reviews = Review.where(id: Task.select(:review_id).group(:review_id).having("count(review_id)>=?", 1))
-    @reviews = has_task_reviews.where(user_id: current_user.id,).page(params[:page]).per(5).order("created_at DESC").includes(:book, :tasks)
+    @reviews = has_task_reviews.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC").includes(:book, :tasks)
   end
 
   def new; end
