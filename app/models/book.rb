d1c8cd68a6c1c10d4ef601da4704c3c9095bd51e
t-krt <cest1la6vieg@gmail.com>
2019-08-11 11:32:52 +0900

    bookモデルのhasmanyオプションを修正
    子要素のレビューが存在するときに、削除しようとするとエラーが発生するようにしました。

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :restrict_with_error
   accepts_nested_attributes_for :reviews
   validates :title, presence: true
 
