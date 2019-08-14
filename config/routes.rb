e17c87f27d5eaedcf10f1d1a3d04373cc966ad62
t-krt <cest1la6vieg@gmail.com>
2019-08-14 15:51:59 +0900

    Update routes.rb
    memberオプションを使用して、ルーティングをリファクタリングしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   root 'top#index'
   devise_for :users
+  
   resources :users, only: [:index] do
+    member do 
+      get :reading, :read, :stock
+    end
     resources :tasks, only: [:index, :new, :create]
   end
+
   resources :reviews
+  
   resources :books, only: :show do
     get :search, on: :collection
   end
-
-  get 'users/:id/reading', to: 'users#reading', as: :reading
-  get 'users/:id/read', to: 'users#read', as: :read
-  get 'users/:id/stock', to: 'users#stock', as: :stock
 end
