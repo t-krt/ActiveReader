7ea8ae4992b5cd1dd967b20619f02bcb2c4c8a86
t-krt <cest1la6vieg@gmail.com>
2019-08-14 15:43:31 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'top#index'
   devise_for :users
   resources :users, only: [:index] do
-    resources :tasks, only: %i[index new create]
+    resources :tasks, only: [:index, :new, :create]
   end
   resources :reviews
   resources :books, only: :show do
