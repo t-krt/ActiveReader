31bcd5f69f3488cb1330317c40442959eebcc5cb
t-krt <cest1la6vieg@gmail.com>
2019-08-15 20:33:56 +0900

    Update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,12 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    has_task_reviews = Review.where(id: Task.select(:review_id).group(:review_id).having("count(review_id)>=?", 1))
-    @reviews = has_task_reviews.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC").includes(:book, :tasks)
+    reviews = Review.where(user_id: current_user.id).order("reviews.created_at DESC").includes(:book, :tasks)
+    @unfinished_task_reviews = reviews.where(tasks: {finished: 0}).page(params[:page]).per(5)    
+    @finished_task_reviews = reviews.where(tasks: {finished: 1}).page(params[:page]).per(5)
+
+    # has_task_reviews = Review.where(id: Task.select(:review_id).group(:review_id).having("count(review_id)>=?", 1))
+    # @reviews = has_task_reviews.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC").includes(:book, :tasks)  
   end
 
   def new; end
