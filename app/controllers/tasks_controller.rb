3941f48577971af6e04b435e964c14ea90cbb618
t-krt <cest1la6vieg@gmail.com>
2019-08-23 18:06:54 +0900

    Update tasks_controller.rb
    タスクを登録するためのアクションを追加しました。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,9 +7,20 @@ class TasksController < ApplicationController
     @finished_task_reviews = reviews.where(tasks: {finished: 1}).page(params[:page]).per(5)
   end
 
-  def new; end
-
-  def create
+  def new
+    @task = Task.new
   end
 
+  def create
+    @task = Task.create(task_params)
+    respond_to do |format|
+      format.html {redirect_to root_path }
+      format.json
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:content, :limit).merge(user_id: current_user.id)
+  end
 end
