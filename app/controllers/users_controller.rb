b27d92d46d64ea8d2aa5fb1fa99e534805c557e4
t-krt <cest1la6vieg@gmail.com>
2019-09-11 23:58:23 +0900

    add: usersControllerの不要なincludesを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,14 @@ class UsersController < ApplicationController
   # end
 
   def reading
-    @reading_book = Review.includes(:book, :user).find_by(user_id: current_user.id, review_status: "reading")
+    @reading_book = Review.includes(:book).find_by(user_id: current_user.id, review_status: "reading")
   end
 
   def read
-    @read_books = Review.includes(:book, :user).where(user_id: current_user.id, review_status: "read").page(params[:page]).per(6).order("created_at DESC")
+    @read_books = Review.includes(:book).where(user_id: current_user.id, review_status: "read").page(params[:page]).per(6).order("created_at DESC")
   end
 
   def stock
-    @stock_books = Review.includes(:book, :user).where(user_id: current_user.id, review_status: "stock").page(params[:page]).per(6).order("created_at DESC")
+    @stock_books = Review.includes(:book).where(user_id: current_user.id, review_status: "stock").page(params[:page]).per(6).order("created_at DESC")
   end
 end
