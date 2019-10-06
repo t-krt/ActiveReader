0c6895a9188d1362c8c0b5ff4b1365563a1dd362
t-krt <cest1la6vieg@gmail.com>
2019-10-06 18:42:06 +0900

    Update avatar_uploader.rb
    画像保存先に関する条件分岐を追加しました。

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,8 +5,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [200, 200]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production? || Rails.env.staging?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
