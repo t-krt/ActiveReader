94a50b3f6650d94d97816ff9d14ad905fa512d11
t-krt <cest1la6vieg@gmail.com>
2019-10-06 23:25:35 +0900

    add: 読書状況ごとのscopeを定義

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,10 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
+  scope :reading, -> { find_by(review_status: "reading") }
+  scope :read, -> { where(review_status: "read") }
+  scope :stock, -> { where(review_status: "stock") }
+
   validates :purpose, presence: true
   validates :review_status, presence: true
   validates :user_id, presence: true
