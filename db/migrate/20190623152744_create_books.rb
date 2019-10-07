4b39b3c4b00500b50aa3f99d9c13d35f6495c12e
t-krt <cest1la6vieg@gmail.com>
2019-10-07 23:43:58 +0900

    fix: create_booksの修正

--- db/migrate/20190623152744_create_books.rb
+++ db/migrate/20190623152744_create_books.rb
@@ -3,8 +3,9 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.string :title, null: false
       t.string :author, null: false
-      t.string :image
-      t.text :detail
+      t.string :image_url
+      t.string :url
+      t.string :isbn, unique: true
       t.timestamps
     end
   end
