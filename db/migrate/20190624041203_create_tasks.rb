2781a3b68d53fe0a35077ef4153b140450b31f4f
t-krt <cest1la6vieg@gmail.com>
2019-10-07 23:43:48 +0900

    fix: create_tasksの修正

--- db/migrate/20190624041203_create_tasks.rb
+++ db/migrate/20190624041203_create_tasks.rb
@@ -1,8 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :task_content, null: false
+      t.string :content, null: false
       t.boolean :finished, default: false, null: false
+      t.date :limit
       t.references :review, foreign_key: true
       t.timestamps
     end
