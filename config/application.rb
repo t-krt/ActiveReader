371fda671e9575e656d8a06e747cba1edacc49b2
t-krt <cest1la6vieg@gmail.com>
2019-10-12 12:48:34 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ module App
       g.javascripts false
       g.helper false
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
