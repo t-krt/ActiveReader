94045e3d46e4cd3121f2b2d6982873654939f782
t-krt <cest1la6vieg@gmail.com>
2019-10-12 21:52:33 +0900

    add: ログイン処理のテストを記述

--- spec/features/review_spec.rb
+++ spec/features/review_spec.rb
@@ -7,5 +7,13 @@ RSpec.feature "Reviews", type: :feature do
     # ログイン前には"本を登録する"ボタンがない
     visit root_path
     expect(page).to have_no_content('本を登録する')
+
+    # ログイン処理
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    first(:css, 'input[name="commit"]').click
+    expect(current_path).to eq reading_user_path(user)
+    expect(page).to have_content('本を登録する')
   end
 end
