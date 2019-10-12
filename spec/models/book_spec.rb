74ea85c4201aa99eda0364abe41bb3298c8fb808
t-krt <cest1la6vieg@gmail.com>
2019-10-12 15:55:43 +0900

    add: bookモデルのバリデーションをテスト

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   # タイトル、著者があれば有効な状態であること
-  it "is valid with a title and author"
-  
+  it "is valid with a title and author" do
+    expect(FactoryBot.build(:book)).to be_valid
+  end
+
   # タイトルがなければ無効な状態であること
-  it "is invalid without a title"
+  it "is invalid without a title" do
+    book = FactoryBot.build(:book, title: nil)
+    book.valid?
+    expect(book.errors[:title]).to include("を入力してください")
+  end
 
   # 著者がなければ無効な状態であること
-  it "is invalid without a title"
+  it "is invalid without a author" do
+    book = FactoryBot.build(:book, author: nil)
+    book.valid?
+    expect(book.errors[:author]).to include("を入力してください")
+  end
 end
