57b494161c150cf5bdec9a7336814489504af83a
t-krt <cest1la6vieg@gmail.com>
2019-10-22 10:59:36 +0900

    fix: user.rbモデルのアソシエーションを修正
    has_many :reviewsに対するdependentオプションを変更しました。
    現状の仕様では、孫に当たるTaskモデルのレコードが存在する場合に、ユーザーを削除できない仕様となっていたためです。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :avatar, AvatarUploader
 
-  has_many :reviews, dependent: :delete_all
+  has_many :reviews, dependent: :destroy
   validates :nickname, presence: true, uniqueness: true
 end
