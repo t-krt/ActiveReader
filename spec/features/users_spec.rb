4b87f37d08bd57dc977996c9aabe80628ea4c3d0
t-krt <cest1la6vieg@gmail.com>
2019-10-22 19:00:19 +0900

    modify: rubocopによるチェック箇所の自動修正

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -8,13 +8,13 @@ RSpec.feature "Users", type: :feature do
   scenario "user creates a new account" do
     visit root_path
     click_link "新規登録"
-    expect {
+    expect do
       fill_in 'user_nickname', with: user.nickname
       fill_in 'user_email', with: user.email
       fill_in 'user_password', with: user.password
       fill_in 'user_password_confirmation', with: user.password
       click_on('登録する')
-    }.to change(User, :count).by(1)
+    end.to change(User, :count).by(1)
     expect(page).to have_content "アカウント登録が完了しました。"
   end
 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,9 @@
 def sign_in_as(user)
-    visit root_path
-    first(:link, "ログイン").click
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
-    first(:css, 'input[name="commit"]').click
+  visit root_path
+  first(:link, "ログイン").click
+  fill_in 'user_email', with: user.email
+  fill_in 'user_password', with: user.password
+  first(:css, 'input[name="commit"]').click
 end
 
 def sign_in_as_test_user
