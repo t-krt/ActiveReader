2dd03d79149efe2ac69ce831c7d050866ea0b3d6
t-krt <cest1la6vieg@gmail.com>
2019-10-22 15:27:33 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, class: User do
     sequence(:nickname)  { |n| "test-user#{n}" }
     sequence(:email)     { |n| "test-user#{n}@example.com" }
     password             { "test123" }
+  end
+
+  factory :sample_user, class: User do
+    sequence(:nickname)  { "sample-user" }
+    sequence(:email)     { "sample-user@example.com" }
+    password             { "sample123" }
   end
 end
