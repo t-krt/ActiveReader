8f6292b2c1229610ea65cede1b83657c0df1fd1c
t-krt <cest1la6vieg@gmail.com>
2019-10-22 17:25:39 +0900

    add: ポートフォリオ閲覧用ユーザーでログインするメソッドを定義

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -5,3 +5,11 @@ def sign_in_as(user)
     fill_in 'user_password', with: user.password
     first(:css, 'input[name="commit"]').click
 end
+
+def sign_in_as_test_user
+  visit root_path
+  first(:link, "ログイン").click
+  fill_in 'user_email', with: 'guest-user@guest.com'
+  fill_in 'user_password', with: 'guest123'
+  first(:css, 'input[name="commit"]').click
+end
