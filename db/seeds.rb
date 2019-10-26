85df9e0b82ae294afd8702a777ed1def1ea8e79b
t-krt <cest1la6vieg@gmail.com>
2019-10-27 02:08:06 +0900

    modify: Rubocopによるコードチェック箇所の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,14 +39,14 @@ Review.create!(
       rate: 5,
       review_status: 1,
       user_id: 1,
-      book_id: 1,
+      book_id: 1
     },
     {
       purpose: "sample_JavaScript",
       rate: 5,
       review_status: 1,
       user_id: 1,
-      book_id: 2,
+      book_id: 2
     }
   ]
 )
