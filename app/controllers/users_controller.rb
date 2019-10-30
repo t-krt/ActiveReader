4985945f337a0884449cd66676d0bce99b28e96c
t-krt <cest1la6vieg@gmail.com>
2019-10-30 19:51:51 +0900

    Update users_controller.rb
    ユーザーごとの情報を取得できるようインスタンス変数を変更しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,21 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
-  # def show
-  #   @reading_book = Review.includes(:book, :user).find_by(user_id: current_user.id, review_status: "reading")
-  # end
+  before_action :set_user
 
   def reading
-    @reading = Review.reading.with_book.find_by(user_id: current_user.id)
+    @reading = Review.reading.with_book.find_by(user_id: params[:id])
   end
 
   def read
-    @reads = Review.read.with_book.desc.where(user_id: current_user.id).page(params[:page]).per(6)
+    @reads = Review.read.with_book.desc.where(user_id: params[:id]).page(params[:page]).per(6)
   end
 
   def stock
-    @stocks = Review.stock.with_book.desc.where(user_id: current_user.id).page(params[:page]).per(6)
+    @stocks = Review.stock.with_book.desc.where(user_id: params[:id]).page(params[:page]).per(6)
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
