bc175cdc3cf031c3b8032a6c300840f5799c1748
t-krt <cest1la6vieg@gmail.com>
2019-11-02 00:07:50 +0900

    add: Likeモデルにvalidateを追加

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :review
   belongs_to :user
+
+  validate do
+    unless user && user.likable_for?(review)
+      errors.add(:base, :invalid)
+    end
+  end
 end
