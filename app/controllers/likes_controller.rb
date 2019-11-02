cf3c532b0c55ceb99efb54ec6d0a2bce399c3240
t-krt <cest1la6vieg@gmail.com>
2019-11-02 19:50:10 +0900

    add: likes_controllerにcreateアクションとdestroyアクションを追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,25 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, only: %i[index create destroy]
+
+  def create
+    @review = Review.find(params[:review_id])
+    unless @review.after_like?(current_user)
+      @review.like(current_user)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    @review = Like.find(params[:id]).review
+    if @review.after_like?(current_user)
+      @review.unlike(current_user)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
+  end
 end
