a4c7ea18f09de93de70fad4bc6b489768bd949a9
t-krt <cest1la6vieg@gmail.com>
2019-11-03 17:57:08 +0900

    add: likes_controllerのindexアクションを追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,13 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!, only: %i[index create destroy]
 
+  def index
+    # ユーザーがお気に入り登録したレビューのidを取得
+    like_ids = Like.where(user_id: current_user.id).pluck(:review_id)
+    # N+1問題への対応のため、予め必要な情報を入れておく
+    @reviews = Review.with_book_user_likers.where(id: like_ids).page(params[:page]).per(5)
+  end
+
   def create
     @review = Review.find(params[:review_id])
     unless @review.after_like?(current_user)
